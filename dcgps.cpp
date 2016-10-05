def6594696c1b73f939ca5c36421544c626f3232
Matt Goerwell <superninja246@hotmail.com>
2016-10-05 16:19:39 -0700

    Updating files to include default headers that explain the purpose of each. This will hopefully give us guidance during the design stage.

---
+++ dcgps.cpp
@@ -0,0 +1,25 @@
+/*------------------------------------------------------------------------------------------------------------------
+--	SOURCE FILE:	dcgps.cpp -	An application that will create a basic gps tracker for
+--					a raspberry PI				
+--
+--	PROGRAM:		dcGPS
+--
+--	FUNCTIONS:
+--					int Main ()
+--
+--	DATE:			October 5, 2016
+--
+--	REVISIONS:		Initial version - October 5, 2016
+--
+--	DESIGNER:		TBD
+--
+--	PROGRAMMER:		TBD
+--
+--	NOTES:
+--	This file will contain the main function only. It will allocate the gps_data_t structure, open the stream to gpsd,
+--  and then start the stream. It will also perform any clean-up or freeing operations upon application termination.
+----------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+    
+}
---
+++ gps-utils.cpp
@@ -0,0 +1,24 @@
+/*------------------------------------------------------------------------------------------------------------------
+--	SOURCE FILE:	gps-utils.cpp -	this file will read data from the gps sensors and send it to
+--					be proccessed for display to the user.	
+--
+--	PROGRAM:		searchGPS
+--
+--	FUNCTIONS:
+--					void gpsLoop()
+--                  void sendToPrint()
+--                  void errorCheck()
+--
+--	DATE:			October 5, 2016
+--
+--	REVISIONS:		Initial version - October 5, 2016
+--
+--	DESIGNER:		TBD
+--
+--	PROGRAMMER:		TBD
+--
+--	NOTES:
+--	This file will conatin at least two functions. One that continuously reads data from the GPS sensors,
+--  and one that sends the data from those sensors to be printed on screen. It is also recommended to have
+--  an error checking function in this file.
+----------------------------------------------------------------------------------------------------------------------*/
